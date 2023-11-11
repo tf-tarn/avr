ad58d9cf13562e7b1885be40a1084bb712a85770
tarn <tarn@elara>
2023-11-11 12:15:28 +0900

    Clean up; reply to PPS message

--- pps/monitor.c
+++ pps/monitor.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-    int ttyfd = open(argv[1], O_RDONLY);
+    int ttyfd = open(argv[1], O_RDWR);
 
     fd_set read_set;
     fd_set exc_set;
@@ -18,7 +18,6 @@ int main(int argc, char **argv) {
     struct timespec last_tp;
 
     while (1) {
-    
         FD_ZERO(&read_set);
         FD_SET(ttyfd, &read_set);
         FD_SET(ttyfd, &exc_set);
@@ -29,38 +28,43 @@ int main(int argc, char **argv) {
             return __LINE__;
         }
 
-        if (n > 0) {
-            if (FD_ISSET(ttyfd, &read_set)) {
-                char b;
-                int n = read(ttyfd, &b, 1);
-                if (n == -1) {
-                    perror("read");
-                    return __LINE__;
-                }
-                if (n > 0 && b == 'U') {
-                    if (clock_gettime(CLOCK_MONOTONIC, &tp) == -1) {
-                        perror("clock_gettime");
-                        return __LINE__;
-                    }
+        if (n < 1) {
+            continue;
+        }
 
-                    time_t diff_sec = tp.tv_sec - last_tp.tv_sec;
-                    long   diff_nsec = tp.tv_nsec - last_tp.tv_nsec;
-                    last_tp = tp;
-
-                    if (diff_nsec < 0) {
-                        diff_sec -= 1;
-                        diff_nsec += 1000000000L;
-                    }
-                    
-                    /* printf("tick tock: %lds, %ldns\n", tp.tv_sec, tp.tv_nsec); */
-                    printf("tick tock: %lds, %ldns\n", diff_sec, diff_nsec);
-                }
-            }
-
-            if (FD_ISSET(ttyfd, &exc_set)) {
-                fprintf(stderr, "exception on file descriptor");
+        if (FD_ISSET(ttyfd, &read_set)) {
+            char b;
+            int n = read(ttyfd, &b, 1);
+            if (n == -1) {
+                perror("read");
                 return __LINE__;
             }
+            if (n > 0 && b == 'U') {
+                if (clock_gettime(CLOCK_MONOTONIC, &tp) == -1) {
+                    perror("clock_gettime");
+                    return __LINE__;
+                }
+
+                time_t diff_sec = tp.tv_sec - last_tp.tv_sec;
+                long   diff_nsec = tp.tv_nsec - last_tp.tv_nsec;
+                last_tp = tp;
+
+                if (diff_nsec < 0) {
+                    diff_sec -= 1;
+                    diff_nsec += 1000000000L;
+                }
+
+                /* printf("tick tock: %lds, %ldns\n", tp.tv_sec, tp.tv_nsec); */
+                printf("tick tock: %lds, %ldns\n", diff_sec, diff_nsec);
+
+                b = ~b;
+                write(ttyfd, &b, 1);
+            }
+        }
+
+        if (FD_ISSET(ttyfd, &exc_set)) {
+            fprintf(stderr, "exception on file descriptor");
+            return __LINE__;
         }
     }
 
