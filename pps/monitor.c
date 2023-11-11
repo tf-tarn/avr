ab57e2a9766ec76fca308bfc7d179b8c66d98aa2
tarn <tarn@elara>
2023-11-11 10:59:30 +0900

    Add PPS monitoring program; update gitignore

---
+++ pps/monitor.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <time.h>
+#include <sys/select.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+    int ttyfd = open(argv[1], O_RDONLY);
+
+    fd_set read_set;
+    fd_set exc_set;
+
+    struct timeval timeout;
+    timeout.tv_sec = 2;
+    timeout.tv_usec = 3;
+
+    struct timespec tp;
+    struct timespec last_tp;
+
+    while (1) {
+    
+        FD_ZERO(&read_set);
+        FD_SET(ttyfd, &read_set);
+        FD_SET(ttyfd, &exc_set);
+        const int n = select(ttyfd + 1, &read_set, NULL, &exc_set, &timeout);
+
+        if (n == -1) {
+            perror("select");
+            return __LINE__;
+        }
+
+        if (n > 0) {
+            if (FD_ISSET(ttyfd, &read_set)) {
+                char b;
+                int n = read(ttyfd, &b, 1);
+                if (n == -1) {
+                    perror("read");
+                    return __LINE__;
+                }
+                if (n > 0 && b == 'U') {
+                    if (clock_gettime(CLOCK_MONOTONIC, &tp) == -1) {
+                        perror("clock_gettime");
+                        return __LINE__;
+                    }
+
+                    time_t diff_sec = tp.tv_sec - last_tp.tv_sec;
+                    long   diff_nsec = tp.tv_nsec - last_tp.tv_nsec;
+                    last_tp = tp;
+
+                    if (diff_nsec < 0) {
+                        diff_sec -= 1;
+                        diff_nsec += 1000000000L;
+                    }
+                    
+                    /* printf("tick tock: %lds, %ldns\n", tp.tv_sec, tp.tv_nsec); */
+                    printf("tick tock: %lds, %ldns\n", diff_sec, diff_nsec);
+                }
+            }
+
+            if (FD_ISSET(ttyfd, &exc_set)) {
+                fprintf(stderr, "exception on file descriptor");
+                return __LINE__;
+            }
+        }
+    }
+
+    return 0;
+}
