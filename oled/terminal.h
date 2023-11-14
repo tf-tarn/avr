16c87598b8f0c982f93e6ad7290cfa293a26e083
tarn <tarn@elara>
2023-11-14 21:06:47 +0900

    Don't use unsigned

--- oled/terminal.h
+++ oled/terminal.h
@@ -1,6 +1,6 @@
 #include <stdlib.h>
-void term_write(unsigned char c);
-void term_write_sn(const unsigned char *s, size_t len);
+void term_write(char c);
+void term_write_sn(const char *s, size_t len);
 void term_write_n(int n);
-void term_write_s(const unsigned char *s);
+void term_write_s(const char *s);
 void term_clear();
